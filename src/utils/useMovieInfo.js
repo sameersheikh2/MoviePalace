225265144ad5d860c87b0794f5b9cbb278d045e4
sameer sheikh <sameersheikh819@gmail.com>
2024-03-24 17:05:27 +0530

    --allow-empty

--- src/utils/useMovieInfo.js
+++ src/utils/useMovieInfo.js
@@ -1,35 +1,35 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// const useMovieInfo = (movie_id) => {
-//   const [movieDetail, setMovieDetail] = useState({});
+const useMovieInfo = (movie_id) => {
+  const [movieDetail, setMovieDetail] = useState({});
 
-//   useEffect(() => {
-//     const fetchMovieDetail = async () => {
-//       const options = {
-//         method: "GET",
-//         headers: {
-//           accept: "application/json",
-//           Authorization: "Bearer " + process.env.MY_BEARER_TOKEN,
-//         },
-//       };
-//       try {
-//         const res = await fetch(
-//           "https://api.themoviedb.org/3/movie/" +
-//             movie_id +
-//             "?append_to_response=videos%2Cimages%2Ccredits%2Csimilar",
-//           options
-//         );
-//         const data = await res.json();
-//         window.scrollTo(0, 0);
-//         setMovieDetail(data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchMovieDetail();
-//   }, [movie_id]);
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: "Bearer " + process.env.MY_BEARER_TOKEN,
+        },
+      };
+      try {
+        const res = await fetch(
+          "https://api.themoviedb.org/3/movie/" +
+            movie_id +
+            "?append_to_response=videos%2Cimages%2Ccredits%2Csimilar",
+          options
+        );
+        const data = await res.json();
+        window.scrollTo(0, 0);
+        setMovieDetail(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMovieDetail();
+  }, [movie_id]);
 
-//   return movieDetail;
-// };
+  return movieDetail;
+};
 
-// export default useMovieInfo;
+export default useMovieInfo;
--- src/utils/useMovies.js
+++ src/utils/useMovies.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const useMovies = (type) => {
-  const [movies, setMovies] = useState([]);
-  const bearerToken = process.env.MY_BEARER_TOKEN;
+// const useMovies = (type) => {
+//   const [movies, setMovies] = useState([]);
+//   const bearerToken = process.env.MY_BEARER_TOKEN;
 
-  const fetchMovies = async () => {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${bearerToken}`,
-      },
-    };
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${type}?language=en-US&page=1`,
-        options
-      );
-      const data = await res.json();
-      setMovies((prevMovies) => [...prevMovies, ...data.results]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//   const fetchMovies = async () => {
+//     const options = {
+//       method: "GET",
+//       headers: {
+//         accept: "application/json",
+//         Authorization: `Bearer ${bearerToken}`,
+//       },
+//     };
+//     try {
+//       const res = await fetch(
+//         `https://api.themoviedb.org/3/movie/${type}?language=en-US&page=1`,
+//         options
+//       );
+//       const data = await res.json();
+//       setMovies((prevMovies) => [...prevMovies, ...data.results]);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  useEffect(() => {
-    fetchMovies();
-  }, []);
-  return movies;
-};
+//   useEffect(() => {
+//     fetchMovies();
+//   }, []);
+//   return movies;
+// };
 
-export default useMovies;
+// export default useMovies;
